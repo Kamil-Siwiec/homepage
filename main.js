988362ea7d9d95675e2960e3941c170ebfab7a2c
Kamil Siwiec <kamsiwcode@gmail.com>
2021-06-16 23:12:38 +0100

    classes & css styles added plus js

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ const age = 32;
 console.log(
   `Witaj Drogi Odwiedzający, nazywam się ${firstName} i mam ${age}lata.`
 );
+
+
+
+
+
+const heading = document.querySelector(".main__heading--js");
+
+heading.innerHTML = `Witaj Drogi Odwiedzający, nazywam się ${firstName} i mam ${age}lata.`;
+
+
+
+const week = "tygodni";
+const number = 5
+
+const emptyParagraph = document.querySelector(".week-summary__description--js");
+
+emptyParagraph.innerHTML = `Nawet uzupełniłem treśc javascriptem, czeka mnie jeszcze ${number} ${week}`;
