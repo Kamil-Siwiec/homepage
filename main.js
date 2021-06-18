99bc47f714b095e93d08d066f4902935b0540942
Kamil Siwiec <kamsiwcode@gmail.com>
2021-06-18 19:07:48 +0100

    js: functions & objects

--- main.js
+++ main.js
@@ -1,30 +1,120 @@
 const firstName = "Maciek";
 const age = 32;
 
-console.log(
-  `Witaj Drogi Odwiedzający, nazywam się ${firstName} i mam ${age}lata.`
-);
 
-
-
-
-
-const heading = document.querySelector(".main__heading--js");
+const heading = document.querySelector('.main__heading--js');
 
 heading.innerHTML = `Witaj Drogi Odwiedzający, nazywam się ${firstName} i mam ${age}lata.`;
 
 
 
-const emptyParagraph = document.querySelector(".week-summary__description--js");
+const emptyParagraph = document.querySelector('.week-summary__description--js');
 
 emptyParagraph.innerHTML = `Nawet uzupełniłem treśc javascriptem!`;
 
-
+ 
 
 function calculate (myNumber) {
-    console.log(`Dostałam ${myNumber}`);
-    return myNumber*7;
+console.log(`Dostałam ${myNumber}`);
+myNumber = myNumber * 7;
+return myNumber;
 }
 
 const myResult = calculate(1);
 console.log(myResult);
+
+
+
+
+
+function greetOld(age, firstName) {
+console.log(`Witaj!, nazywam się ${firstName} i mam ${age} lata`);
+}
+
+greetOld(age,firstName)
+
+
+
+
+
+function createContent(querySelectorContent, content) {
+const element = document.querySelector(querySelectorContent);
+element.innerHTML = content;
+}
+
+createContent('.week-summary__description--js','Siema siema');
+
+createContent('.week-summary__title--js', 'Podmieniony tytuł');
+
+
+
+
+function HelloWorld () {
+console.log('witaj świecie');
+}
+
+HelloWorld();
+
+
+
+
+const greet = (age, firstName) => {
+console.log(`Witaj Drogi Odwiedzający, nazywam sie${firstName} i mam ${age}lata`);
+
+}
+greet(age, firstName);
+
+
+
+
+function calculateOld(myNumber) {
+    return myNumber*7;
+}
+
+
+
+
+
+const calculateNew = (myNumber) => myNumber*7
+
+const myResultNew = calculateNew(7)
+console.log(myResultNew)
+
+
+
+
+const deathStar = {
+ diameter: 120000,
+ fire: (target) => {
+     console.log(`${target} destroyed`)
+ },
+ isOperating: true,
+ levels: 357,
+ name: 'Death Star',
+ population: 10000,
+ isLightOn: true,
+ commander: {
+     name: 'Darth Vader',
+     age: 44
+ }
+}
+
+console.log(deathStar.commander.name);
+
+console.log(console);
+console.log(typeof deathStar.name)
+
+deathStar.fire('Rebel Ship');
+
+
+console.log(deathStar['name'])
+
+
+
+
+const myProperty = 'name'
+
+const showMeProperty = (myProperty) => {
+    console.log(`Twoja własność ${myProperty} to: ${deathStar[myProperty]}`);
+}
+showMeProperty('levels')
